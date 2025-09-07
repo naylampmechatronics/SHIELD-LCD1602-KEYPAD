6c98d3beb3ce071e4900c35b284a839145bb2794
David S. Cast. <dastcast@gmail.com>
2025-09-06 21:10:06 -0500

    Update shield_lcd1602_keypad_test_1.ino

--- shield_lcd1602_keypad_test_1.ino
+++ shield_lcd1602_keypad_test_1.ino
@@ -17,6 +17,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
   // Print a message to the LCD.
   lcd.print("hola mundo - NM!");
 }
@@ -31,23 +32,24 @@ void loop() {
   int x;
   x = analogRead (0);
   lcd.setCursor(10,1);
+
   if (x < 60) {
-    lcd.print ("Right ");
+    lcd.print("Right ");
   }
   else if (x < 200) {
-    lcd.print ("Up    ");
+    lcd.print("Up    ");
   }
   else if (x < 400){
-    lcd.print ("Down  ");
+    lcd.print("Down  ");
   }
   else if (x < 600){
-    lcd.print ("Left  ");
+    lcd.print("Left  ");
   }
   else if (x < 800){
-    lcd.print ("Select");
+    lcd.print("Select"); 
   }
   else if (x < 1024){
-    lcd.print ("      ");
+    lcd.print("N/A   ");
   }
 
 }
